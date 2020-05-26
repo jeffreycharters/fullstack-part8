b380786702ac7370ca0ebc4600e636ec7b4dfb1d
dietBlue <jeff.charters@gmail.com>
2020-05-26 09:47:55 -0400

    finished ex8.9

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,52 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 
-import { useQuery } from '@apollo/client'
+import { useQuery, useLazyQuery } from '@apollo/client'
 
 import { ALL_AUTHORS } from './queries'
+import { ALL_BOOKS } from './queries'
 
 const App = () => {
   const [page, setPage] = useState('authors')
+  const [getBooks, bookResult] = useLazyQuery(ALL_BOOKS)
+  const [books, setBooks] = useState([])
 
-  const result = useQuery(ALL_AUTHORS)
+  const authorResult = useQuery(ALL_AUTHORS)
 
-  if (result.loading) {
+  useEffect(() => {
+    if (bookResult.data) {
+      setBooks(bookResult.data.allBooks)
+    }
+  }, [bookResult])
+
+  if (authorResult.loading) {
     return <div>loading...</div>
+  }
+
+  const handleBookClick = (event) => {
+    event.preventDefault()
+    setPage('books')
+    getBooks()
   }
 
   return (
     <div>
       <div>
         <button onClick={() => setPage('authors')}>authors</button>
-        <button onClick={() => setPage('books')}>books</button>
+        <button onClick={handleBookClick}>books</button>
         <button onClick={() => setPage('add')}>add book</button>
       </div>
 
       <Authors
         show={page === 'authors'}
-        authors={result.data.allAuthors}
+        authors={authorResult.data.allAuthors}
       />
 
       <Books
         show={page === 'books'}
+        books={books}
       />
 
       <NewBook
